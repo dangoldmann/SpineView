4965d9fe2925d0de28ed88f66f1cb80c54e485b4
dangoldmann <101563504+dangoldmann@users.noreply.github.com>
2022-08-02 15:54:15 -0300

    fix: corrected the delete method

--- Back-End/index.js
+++ Back-End/index.js
@@ -6,19 +6,23 @@ app.use(express.json())
 const users = [
     {id:1, name:"Dan"},
     {id:2, name:"Ian"},
-    {id:3, name:"Ivo"}
+    {id:3, name:"Ivo"},
+    {id:4, name:"Pancho"}
 ]
 
 app.get('/api/users', (req, res) => {
-    res.send(users);
+    res.json(users)
+    //res.send(users)
 })
 
 app.get('/api/users/:id', (req, res) => {
     const user = users.find(c => c.id === parseInt(req.params.id))
+    
     if(!user)
     {
         res.status(404).send('User not found')
     }
+    
     res.send(user)
 })
 
@@ -34,8 +38,18 @@ app.post('/api/users', (req, res) => {
         id: users.length + 1,
         name: req.body.name
     }
+    
     users.push(user)
-    res.send(user)
+    res.status(201).send()
+})
+
+app.post('/api/users/login', (req, res) => {
+    const user = users.find(user => user.name = req.body.name)
+
+    if(user == null)
+    {
+        return res.status(400).send('Cannot find user')
+    }
 })
 
 app.put('/api/users/:id', (req, res) => {
@@ -58,9 +72,9 @@ app.delete('/api/users/:id', (req, res) => {
     if(!user) res.status(404).send('User not found')
 
     const index = users.indexOf(user)
-    user.splice(index, 1)
+    users.splice(index, 1)
 
-    res.send(user)
+    res.send('The user has been deleted successfully')
 })
 
 app.listen(3000, () => console.log(`Listening on port 3000...`))
