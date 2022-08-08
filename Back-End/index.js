41684a63bbe4eae6f6b3b55f7293e8f15c872cf9
dangoldmann <101563504+dangoldmann@users.noreply.github.com>
2022-08-08 11:55:29 -0300

    feat: added login connected to db with password encription

--- Back-End/index.js
+++ Back-End/index.js
@@ -6,15 +6,14 @@ const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "rootroot",
-    database : "osia"
+    database: "osia"
 })
 
 app.use(express.json())
 
 db.connect((err) => {
-    if(err)
-    {
-        throw(err)
+    if (err) {
+        throw (err)
     }
     console.log('MySQL Connected...')
 })
@@ -22,99 +21,100 @@ db.connect((err) => {
 app.get('/api/users', (req, res) => {
     let sql = 'select * from usuario'
     db.query(sql, (err, result) => {
-        if(err) throw err
+        if (err) throw err
         res.send(result)
     })
 })
 
 app.get('/api/users/:id', (req, res) => {
-    const {id} = req.params
+    const { id } = req.params
     let sql = `select * from usuario where id = ${id}`
     db.query(sql, (err, result) => {
-        if(err) throw err
-        
-        if(result.length != 0) res.send(result)
+        if (err) throw err
+
+        if (result.length != 0) res.send(result)
         else res.status(404).send('User not found')
     })
 })
 
 app.post('/api/users', (req, res) => {
-    const {name, surname, email, phone, password} = req.body
-    
+    const { name, surname, email, phone, password } = req.body
+
     let sql = `insert into usuario (nombre, apellido, email, telefono, contraseña) values ('${name}', '${surname}', '${email}', '${phone}', '${password}')`
     db.query(sql, (err, result) => {
-        if(err) throw err
+        if (err) throw err
         res.status(201).send('User created correctly')
     })
 })
 
 app.put('/api/users/:id', (req, res) => {
-    const {id} = req.params
-    const {name} = req.body
+    const { id } = req.params
+    const { name } = req.body
 
     let sql = `update usuario set nombre = '${name}' where id = ${id}`
     db.query(sql, (err, result) => {
-        if(err) throw err
+        if (err) throw err
 
-        if(result.affectedRows != 0) res.send('User updated successfully')
+        if (result.affectedRows != 0) res.send('User updated successfully')
         else res.status(404).send('User not found')
-        
+
     })
 })
 
 app.delete('/api/users/:id', (req, res) => {
-    const {id} = req.params
+    const { id } = req.params
 
     let sql = `delete from usuario where id = ${id}`
     db.query(sql, (err, result) => {
-        if(err) throw err
-        
+        if (err) throw err
+
         if (result.affectedRows != 0) res.send('The user has been deleted successfully')
         else res.status(404).send('User not found')
-        
+
     })
 })
 
-const userLogin = []
+app.post('/api/users/register', async (req, res) => {
+    const { name, surname, email, phone, password } = req.body
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10)
 
-app.get('/api/usersLogin', (req, res) => {
-    res.json(userLogin)
+        let sql = `insert into usuario (nombre, apellido, email, telefono, contraseña) values ('${name}', '${surname}', '${email}', '${phone}', '${hashedPassword}')`
+        db.query(sql, (err, result) => {
+            if (err) throw err
+            res.status(201).send('User created correctly')
+        })
+    }
+    catch { res.status(500).send() }
 })
 
-app.post('/api/usersLogin', async (req, res) => {
-    try
-    {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        const user = {name: req.body.name, password: hashedPassword}
-        userLogin.push(user)
-        res.status(201).send()
-    }
-    catch
-    {
-        res.status(500).send()
-    }
-})
+app.post('/api/users/login', async (req, res) => {
+    const { email, password } = req.body
+    
+    let sql = `select * from usuario where email = '${email}'`
+    db.query(sql, async (err, result) => {
+        if (err) throw err
 
-app.post('/api/usersLogin/login', async (req, res) => {
-    const user = userLogin.find(user => user.name == req.body.name)
-    
-    if(!user) return res.status(400).send('Cannot find user')
-    
-    try
-    {
-        if(await bcrypt.compare(req.body.password, user.password))
+        let hashedPassword
+
+        if (result.length != 0) 
         {
-            res.send('Success')
+            hashedPassword = result[0].contraseña
         }
-        else
+        else return res.status(404).send('User not found')
+
+        try 
         {
-            res.send('Not allowed')
+            if (await bcrypt.compare(password, hashedPassword)) {
+                res.send('Success')
+            }
+            else {
+                res.send('Incorrect password')
+            }
         }
-    }
-    catch
-    {
-        res.status(500).send()
-    }
+        catch { res.status(500).send() }
+
+    })
 })
 
 app.listen(3000, () => console.log(`Listening on port 3000...`))
