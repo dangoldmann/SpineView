093c0de5468a2ab39db4805380cbbbdd24ca3f24
IanPol0 <ipolonuer@gmail.com>
2022-08-22 10:58:52 -0300

    Update controller.js

--- Front-End/Pruebas HTML y CSS/controller.js
+++ Front-End/Pruebas HTML y CSS/controller.js
@@ -22,8 +22,8 @@ var a_escanear = document.getElementById("a_escanear");
 
 wrapper.addEventListener("scroll", () => {
     navbar.classList.toggle("navbarAltColor", wrapper.scrollTop>=1 );
-    quienesSomos.classList.toggle("alt", wrapper.scrollTop>600 && wrapper.scrollTop<1531);
-    a_escanear.classList.toggle("alt", wrapper.scrollTop>1531);
+    quienesSomos.classList.toggle("alt", wrapper.scrollTop>600 && wrapper.scrollTop<=1331);
+    a_escanear.classList.toggle("alt", wrapper.scrollTop>1331);
     console.log(wrapper.scrollTop);
 });
 
