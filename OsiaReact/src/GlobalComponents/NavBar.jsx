61942f79bd7cd9703bf145b69c9878e69bc4f1cd
IanPol0 <ipolonuer@gmail.com>
2022-08-23 09:24:28 -0300

    fix: images on the homepage

--- OsiaReact/src/GlobalComponents/NavBar.jsx
+++ OsiaReact/src/GlobalComponents/NavBar.jsx
@@ -1,16 +1,17 @@
-//import {logoOsia} from '../assets/logoOsia.png';
+import imgUsuario from '../assets/Usuario.png'
+import logoOsia from "../assets/logoOsia.png"
 
 function NavBar(){
     return (
         <nav className="navbar" id="NavBar">
-            <button id="btn_usuario" onclick="openSideMenu()"><img id="imgUsuario" src=""/></button>
+            <button id="btn_usuario" onclick="openSideMenu()"><img id="imgUsuario" src={imgUsuario}/></button>
             <ul id="links">
                 <li><a href="#segundaseccion" className="link" id="a_quienesSomos">Quiénes somos</a></li>
                 <li><a className="link" href="IngresarImagen.html" id="a_escanear">Escanear estudio</a></li>
                 <li><a className="link" href="" id="masInfo">Más Información</a></li>
             </ul>
 
-            <a href="" onclick="BackToTop()"><img src="" id="logo"/></a>
+            <a href="" onclick="BackToTop()"><img src={logoOsia} id="logo"/></a>
             
             <div id="MenuLateral" className="menulateral">
                 <img className="imgUsuario" src="../assets/Usuario.png"/>
