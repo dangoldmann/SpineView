d29a94440de2ddaaa0d1800e2b79a70f780d7870
IanPol0 <ipolonuer@gmail.com>
2022-08-26 11:49:13 -0300

    Feat: register form creates a formData to send to the database
    Still need further investigation of what a XhttpRequest is and how to send and receive a formData object

---
+++ Front-End/Pruebas HTML y CSS/SendData.js
@@ -0,0 +1,24 @@
+var form = document.getElementById("formRegistro");
+var output = document.getElementById("output");
+
+form.addEventListener("submit", ()=>{
+    var formData = new FormData(form);
+
+    if(formData.get("ContraseniaUsuario"!=formData.get("ContraseniaUsuarioConfirmar"))){
+        output.innerHTML = "Las contraseñas no coinciden";
+    }
+    else{
+        var req= new XMLHttpRequest();
+        req.open("POST", "/routes/users", true);
+        req.onload = function(){
+            if(req.status == 200){
+                output.innerHTML = "Usuario registrado";
+            }
+            else{
+                output.innerHTML = "Error al registrar usuario";
+            }
+        }
+        req.send(formData);
+        form.submit();
+    }
+})
