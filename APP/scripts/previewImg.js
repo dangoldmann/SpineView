64bf3d29dafc1678c4beddece12c97137fd895af
IanPol0 <ipolonuer@gmail.com>
2022-09-24 15:56:36 -0300

    feat: image preview on the upload image form

--- APP/scripts/previewImg.js
+++ APP/scripts/previewImg.js
@@ -1,4 +1,23 @@
 document.addEventListener("DOMContentLoaded", ()=>{
     const inpFile = document.getElementById("imgInput");
+    const previewContainer = document.getElementById("previews");
+    const previewImage = previewContainer.querySelector(".img-preview__image");
+    const previewDefaultText = previewContainer.querySelector(".img-preview__default-text");
     
+    inpFile.addEventListener("change", function(){
+        const file = this.files[0];
+        if(file){
+            const reader = new FileReader();
+            previewDefaultText.style.display = "none";
+            previewImage.style.display = "block";
+            reader.addEventListener("load", function(){
+                previewImage.setAttribute("src", this.result);
+            });
+            reader.readAsDataURL(file);
+        }else{
+            previewDefaultText.style.display = null;
+            previewImage.style.display = null;
+            previewImage.setAttribute("src", "");
+        }
+    });
 });
