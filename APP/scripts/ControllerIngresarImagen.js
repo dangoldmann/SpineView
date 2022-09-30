ea79fc10423363a5536a3213d71bde341b33f026
IanPol0 <ipolonuer@gmail.com>
2022-09-30 14:28:11 -0300

    feat: started working on drag n drop functionality
    also added button to reload page when user uploads incorrect img

--- APP/scripts/ControllerIngresarImagen.js
+++ APP/scripts/ControllerIngresarImagen.js
@@ -1,6 +1,36 @@
-var form = document.getElementById("ingresarImagenes");
-var fileInput = document.getElementById("imgInput");
+document.addEventListener("DOMContentLoaded", ()=>{
+    var form = document.getElementById("ingresarImagenes");
+    var fileInput = document.getElementById("imgInput");
+    var dropArea = document.getElementById("ingresarImagenes")
 
-form.addEventListener("click", () =>{
-    fileInput.click();
+    form.addEventListener("click", () =>{
+        fileInput.click();
+    });
+
+    ;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+        dropArea.addEventListener(eventName, preventDefaults, false)
+    })
+      
+    function preventDefaults (e) {
+        e.preventDefault()
+        e.stopPropagation()
+    }
+
+    ;['dragenter', 'dragover'].forEach(eventName => {
+        dropArea.addEventListener(eventName, highlight, false)
+    })
+    
+    ;['dragleave', 'drop'].forEach(eventName => {
+        dropArea.addEventListener(eventName, unhighlight, false)
+    })
+    
+    function highlight(e) {
+        dropArea.classList.add('highlight')
+    }
+    
+    function unhighlight(e) {
+        dropArea.classList.remove('highlight')
+    }
+
+    //take the dropped file and add it to FileInput 
 });
--- APP/scripts/previewImg.js
+++ APP/scripts/previewImg.js
@@ -3,6 +3,8 @@ document.addEventListener("DOMContentLoaded", ()=>{
     const previewContainer = document.getElementById("previews");
     const previewImage = previewContainer.querySelector(".img-preview__image");
     const previewDefaultText = previewContainer.querySelector(".img-preview__default-text");
+    const ingresarImagenes = document.getElementById("ingresarImagenes");
+    const redo = document.getElementById("redo");
     
     inpFile.addEventListener("change", function(){
         const file = this.files[0];
@@ -19,5 +21,9 @@ document.addEventListener("DOMContentLoaded", ()=>{
             previewImage.style.display = null;
             previewImage.setAttribute("src", "");
         }
+
+        ingresarImagenes.style.display = "none";
+        previewContainer.classList.add("bigPreview");
+        redo.style.display = "inline-block";
     });
 });
