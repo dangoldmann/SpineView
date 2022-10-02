c53c116a5fbebfc438ac3ef15bd873907a68b0e2
dangoldmann <goldmandan8@gmail.com>
2022-10-01 23:28:16 -0300

    feat: forgot-password and reset-password files created

---
+++ APP/scripts/forgotPassword.js
@@ -0,0 +1,39 @@
+//const apiUrl = 'https://osia-api-production.up.railway.app'
+const apiUrl = 'http://localhost:3000'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const btn_submit = document.getElementById('btn_submit')
+    const form = document.getElementById('formResetPassword')
+
+    btn_submit.onclick = e => {
+        e.preventDefault()
+        const datosUsuario = new FormData(form)
+
+        forgotPassword(datosUsuario)
+    }
+})
+
+async function forgotPassword(formData) {
+    const url = apiUrl + '/users/forgot-password'
+    const email = formData.get('email')
+
+    let res = await postRequest(url, {email})
+    res = await res.json()
+
+    if(res.error){
+        alert(res.error.message)
+    }
+}
+
+async function postRequest(url, data){
+    const res = await fetch(url, {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+  
+    return res
+}
---
+++ APP/scripts/resetPassword.js
@@ -0,0 +1,37 @@
+//const apiUrl = 'https://osia-api-production.up.railway.app'
+const apiUrl = 'http://localhost:3000'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const btn_submit = document.getElementById("btn_submit");
+    var form = document.getElementById("formLogIn");
+
+    btn_submit.onclick = e => {
+        e.preventDefault()
+        var datosUsuario = new FormData(form)
+
+        resetPassword(datosUsuario)
+    }
+})
+
+async function resetPassword(formData) {
+    const url = apiUrl + '/test/reset-password'
+
+    const password = formData.get('password')
+
+    let res = await postRequest(url, password)
+    res = await res.json()
+
+}
+
+async function postRequest(url, data){
+    const res = await fetch(url, {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+  
+    return res
+}
