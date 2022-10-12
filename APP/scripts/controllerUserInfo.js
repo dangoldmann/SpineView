7a9877f4b46d8eb23473409ebaae11abcdc0c88b
IanPol0 <ipolonuer@gmail.com>
2022-10-12 20:22:15 -0300

    user info and study history are now on the same window

---
+++ APP/scripts/controllerUserInfo.js
@@ -0,0 +1,12 @@
+let openTab = (tabName) => {
+    let i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tab");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("btn");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "flex";
+}   
