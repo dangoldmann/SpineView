1c8faa6bafe12c2d8be8eb648c78d55d2c998548
IanPol0 <ipolonuer@gmail.com>
2022-10-12 19:03:51 -0300

    Create OpenCloseSideMenu.js

---
+++ APP/scripts/OpenCloseSideMenu.js
@@ -0,0 +1,7 @@
+const closeSideMenu=(() =>{
+    document.getElementById('MenuLateral').style.width = '0';
+});
+
+const openSideMenu = (()=>{
+    document.getElementById('MenuLateral').style.width = '300px';
+});
