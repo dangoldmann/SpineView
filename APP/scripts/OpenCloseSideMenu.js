8e950b15bdd595926b0dca70b4537fcec0858c70
IanPol0 <ipolonuer@gmail.com>
2022-10-14 09:38:21 -0300

    fix: mooved openTab to openCloseSideMenu.js function for use

--- APP/scripts/OpenCloseSideMenu.js
+++ APP/scripts/OpenCloseSideMenu.js
@@ -5,3 +5,16 @@ const closeSideMenu=(() =>{
 const openSideMenu = (()=>{
     document.getElementById('MenuLateral').style.width = '300px';
 });
+
+var openTab = (tabName) => {
+    let i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tab");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("btn");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "flex";
+}
--- APP/scripts/controllerUserInfo.js
+++ APP/scripts/controllerUserInfo.js
@@ -22,16 +22,3 @@ async function loadData(){
     lblEmail.textContent = userInfo.email
     lblPhone.textContent = userInfo.phone
 }
-
-let openTab = (tabName) => {
-    let i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tab");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("btn");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(tabName).style.display = "flex";
-}   
