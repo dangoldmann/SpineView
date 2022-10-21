4ec2a051e192d954ced979483dd12022c2802242
IanPol0 <101563167+IanPol0@users.noreply.github.com>
2022-10-21 14:36:50 -0300

    feat: rendering user studies into "studies" tab

--- APP/scripts/getstudies.js
+++ APP/scripts/getstudies.js
@@ -1,25 +1,52 @@
 import {html, render} from 'https://unpkg.com/lit-html?module';
+import {apiUrl} from './config.js'
+import {getRequest} from './http_requests.js'
 
 const studiesTab= document.getElementById("studies")
+var studiesArray=[]
 
-var study = (image, date, result)=> html`
+var createStudyBox=(image, date, result)=>{
+    var studybox = (stdimage, stddate, stdresult)=> html`
     <div class="study">
-    <img src="${image}" alt="">
+    <img src="${stdimage}" alt="">
     <div class="studytext">
-        <h5>Fecha: <span>${date}</span></h5>
-        <h5>Resultado: <span>${result}</span></h5>
+        <h5>Fecha: <span>${stddate}</span></h5>
+        <h5>Resultado: <span>${stdresult}</span></h5>
     </div>
-    </div>
-`;
-
-
-var fillTemplate=(template, image, date, result)=>{
-    return template(image, date, result)
+    </div>`;
+    var completestudybox = studybox(image, date, result)
+    return completestudybox;
 }
 
-var study1 = fillTemplate(study, "../public/images/Home.png", "dd/mm/aa", "Hernia de discos")
-var study2 = fillTemplate(study, "../public/images/Home.png", "dd/mm/aa", "Hernia de discos")
+// async function getStudies(){
+//     const url = apiUrl + "/radiographies"
+//     const res = await getRequest(url)
+//     var allUserStudies = res.studies
+//     return allUserStudies
+// }
 
-var templateArray = [study1, study2]
-render(templateArray, studiesTab)
+// var allUserStudies = getStudies()
 
+var allUserStudies=[
+    {
+        stdimage:"../public/images/columna.jpg",
+        stddate:"xx/xx/xx",
+        stdresult:"Fisura de vertebra"
+    },
+    {
+        stdimage:"../public/images/Home.png",
+        stddate:"xx/xx/xx",
+        stdresult:"Hernia de discos"
+    }
+]
+
+allUserStudies.forEach(el => {
+    var image= el.stdimage
+    var date=el.stddate
+    var result=el.stdresult
+    
+    var studyBox = createStudyBox(image, date, result)
+    studiesArray.push(studyBox)
+});
+
+render(studiesArray, studiesTab)
